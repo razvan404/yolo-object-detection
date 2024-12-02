a4fd4f5aa2fed7e77f329af04803d17e58347549
razvan404 <razvanpetec@gmail.com>
2024-12-02 09:34:23 +0200

    Update YOLO model to create all subsequent directories

--- src/models.py
+++ src/models.py
@@ -6,12 +6,11 @@ from ultralytics import YOLO as BaseYOLO
 from .filesystem import FileSystem
 
 
-def YOLO(model_name: str = 'yolov8s.pt', *, models_path: Path = None) -> BaseYOLO:
+def YOLO(model_name: str = "yolov8s.pt", *, models_path: Path = None) -> BaseYOLO:
     if models_path is None:
         models_path = FileSystem.MODELS_DIR
 
-    if not os.path.exists(models_path):
-        os.mkdir(models_path)
-
+    os.makedirs(models_path, exist_ok=True)
     model_path = models_path / model_name
+
     return BaseYOLO(model=model_path, task="detect", verbose=False)
